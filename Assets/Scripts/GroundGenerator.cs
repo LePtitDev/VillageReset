9caf0d7c70a55107742f6851e15794fc71c7902f
LePtitDev <perso@leptitdev.com>
2017-10-08 17:56:58 +0200

    Premier push du projet

---
+++ Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour {
+
+	// Current instance of GroundGenerator
+	public static GroundGenerator Instance;
+
+	// Random seed
+	[Range(1000000, 1000000000)]
+	public System.Int32 Seed;
+
+	// Width and height of map
+	public int Width, Height;
+
+	// Percentage of void, water and segregation threshold
+	[Range(5, 100)]
+	public int VoidPercentage, WaterPercentage, SegregationThreshold;
+
+	// Grass, water and void prefabs
+	public GameObject GrassPrefab, WaterPrefab, VoidPrefab;
+
+	// Patches map
+	public GameObject[,] Patches;
+
+	// Display or not the patch groups
+	public bool DisplayGroup;
+
+	// Segregate step delay
+	[Range(0.0f, 1.0f)]
+	public float StepDelay;
+
+	// The list of void patches
+	public List<GameObject> VoidList = new List<GameObject>();
+
+	// The list of grass patches
+	public List<GameObject> GrassList = new List<GameObject>();
+
+	// The list of water patches
+	public List<GameObject> WaterList = new List<GameObject>();
+
+	// Indicate if we need to segregate and if we need to transform void patches at the end of segregation
+	public bool NeedSegregate = true, SwapEnding = true;
+
+	// Randomizer
+	public System.Random Randomizer;
+
+	// Timer for segregate steps
+	float time;
+
+	// Pass counter
+	int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		Instance = this;
+		Patches = new GameObject[Width, Height];
+		List<Vector2> voidCoords = new List<Vector2> ();
+		List<Vector2> waterCoords = new List<Vector2> ();
+		List<Vector2> grassCoords = new List<Vector2> ();
+		Vector2[] patches = new Vector2[Width * Height];
+		for (int z = 0; z < Height; z++) {
+			for (int x = 0; x < Width; x++) {
+				patches [z * Width + x] = new Vector2 (x, z);
+			}
+		}
+		Randomizer = new System.Random(Seed);
+		patches = patches.OrderBy(item => Randomizer.Next()).ToArray();
+		int voidCount = patches.Length * VoidPercentage / 100;
+		int waterCount = (patches.Length - voidCount) * WaterPercentage / 100;
+		for (int i = 0; i < voidCount; i++)
+			voidCoords.Add (patches [i]);
+		for (int i = voidCount, sz = voidCount + waterCount; i < sz; i++)
+			waterCoords.Add (patches [i]);
+		for (int i = voidCount + waterCount, sz = patches.Length; i < sz; i++)
+			grassCoords.Add (patches [i]);
+		
+		for (int z = 0; z < Height; z++) {
+			for (int x = 0; x < Width; x++) {
+				GameObject tmp;
+				if (voidCoords.Contains (new Vector2 (x, z))) {
+					tmp = Instantiate (VoidPrefab, new Vector3 (x, 0, z), Quaternion.identity, transform);
+					VoidList.Add (tmp);
+				} else if (waterCoords.Contains (new Vector2 (x, z))) {
+					tmp = Instantiate (WaterPrefab, new Vector3 (x, 0, z), Quaternion.identity, transform);
+					WaterList.Add (tmp);
+				} else {
+					tmp = Instantiate (GrassPrefab, new Vector3 (x, 0, z), Quaternion.identity, transform);
+					GrassList.Add (tmp);
+				}
+				Patches [x, z] = tmp;
+			}
+		}
+		time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (NeedSegregate && Time.time - time > StepDelay) {
+			NeedSegregate = false;
+			List<GameObject> list = new List<GameObject> ();
+			list.AddRange (GrassList);
+			list.AddRange (WaterList);
+			list = list.OrderBy(item => Randomizer.Next()).ToList();
+			foreach (GameObject g in list)
+				g.SendMessage ("Shuffle");
+			time = Time.time;
+
+			count++;
+			if (!NeedSegregate) {
+				Debug.Log ("Segregation terminée en " + count + " passes");
+				if (SwapEnding) {
+					foreach (GameObject g in VoidList) {
+						GameObject tmp;
+						if (g.GetComponent<GroundSegregation> ().GroupMajority () == 1) {
+							tmp = Instantiate (GrassPrefab, g.transform.position, Quaternion.identity, transform);
+							GrassList.Add (tmp);
+						} else {
+							tmp = Instantiate (WaterPrefab, g.transform.position, Quaternion.identity, transform);
+							WaterList.Add (tmp);
+						}
+						Patches [(int)g.transform.position.x, (int)g.transform.position.y] = tmp;
+						Destroy (g);
+					}
+					VoidList.Clear ();
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/GroundSegregation.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSegregation : MonoBehaviour {
+
+	/////////////////////
+	/// ATTRIBUTES //////
+	/////////////////////
+
+	// Material for group displaying
+	private static Material lineMaterial = null;
+
+	// Group ID
+	public int Group;
+
+	//////////////////
+	/// METHODS //////
+	//////////////////
+
+	// Use this for initialization
+	void Start () {
+		CreateLineMaterial();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GroundGenerator.Instance.DisplayGroup) {
+			switch (Group) {
+			case 0:
+				DrawLine (transform.position, transform.position + new Vector3 (0, 1, 0), Color.blue, GroundGenerator.Instance.StepDelay);
+				break;
+			case 1:
+				DrawLine (transform.position, transform.position + new Vector3 (0, 1, 0), Color.red, GroundGenerator.Instance.StepDelay);
+				break;
+			case 2:
+				DrawLine (transform.position, transform.position + new Vector3 (0, 1, 0), Color.green, GroundGenerator.Instance.StepDelay);
+				break;
+			}
+		}
+	}
+
+	// Apply a segregation shuffle if needed
+	void Shuffle() {
+		if (Group != 0) {
+			int Width = GroundGenerator.Instance.Width, Height = GroundGenerator.Instance.Height;
+			int groupCount = 0, otherGroupCount = 0;
+			for (int z = (int)transform.position.z - 1, h = z + 2; z <= h; z++) {
+				for (int x = (int)transform.position.x - 1, w = x + 2; x <= w; x++) {
+					if ((z == h - 1 && x == w - 1) || z < 0 || z >= Height || x < 0 || x >= Width)
+						continue;
+					int other = GroundGenerator.Instance.Patches [x, z].GetComponent<GroundSegregation> ().Group;
+					if (other == Group)
+						groupCount++;
+					else if (other != 0)
+						otherGroupCount++;
+				}
+			}
+			if (otherGroupCount == 0)
+				otherGroupCount++;
+			if (groupCount * 100 / otherGroupCount < GroundGenerator.Instance.SegregationThreshold) {
+				GameObject new_place = GroundGenerator.Instance.VoidList [(int)(GroundGenerator.Instance.Randomizer.NextDouble() * GroundGenerator.Instance.VoidList.Count)];
+				Vector3 tmp = new_place.transform.position;
+				new_place.transform.position = transform.position;
+				GroundGenerator.Instance.Patches [(int)transform.position.x, (int)transform.position.z] = new_place;
+				transform.position = tmp;
+				GroundGenerator.Instance.Patches [(int)transform.position.x, (int)transform.position.z] = this.gameObject;
+				GroundGenerator.Instance.NeedSegregate = true;
+			}
+		}
+	}
+
+	// Return the neighbors group majority
+	public int GroupMajority() {
+		int Width = GroundGenerator.Instance.Width, Height = GroundGenerator.Instance.Height;
+		int grassCount = 0, waterCount = 0;
+		for (int z = (int)transform.position.z - 1, h = z + 2; z <= h; z++) {
+			for (int x = (int)transform.position.x - 1, w = x + 2; x <= w; x++) {
+				if ((z == h - 1 && x == w - 1) || z < 0 || z >= Height || x < 0 || x >= Width)
+					continue;
+				int other = GroundGenerator.Instance.Patches [x, z].GetComponent<GroundSegregation> ().Group;
+				if (other == 1)
+					grassCount++;
+				else if (other == 2)
+					waterCount++;
+			}
+		}
+		return (grassCount >= waterCount) ? 1 : 2;
+	}
+
+	// Draw the patch group
+	void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
+	{
+		GameObject myLine = new GameObject();
+		myLine.transform.position = start;
+		myLine.AddComponent<LineRenderer>();
+		LineRenderer lr = myLine.GetComponent<LineRenderer>();
+		lr.material = lineMaterial;
+		lr.startColor = color;
+		lr.endColor = color;
+		lr.startWidth = 0.1f;
+		lr.endWidth = 0.1f;
+		lr.SetPosition(0, start);
+		lr.SetPosition(1, end);
+		GameObject.Destroy(myLine, duration);
+	}
+
+	// Create the matrial
+	private static void CreateLineMaterial()
+	{
+		if (lineMaterial == null)
+		{
+			// Unity has a built-in shader that is useful for drawing
+			// simple colored things.
+			Shader shader = Shader.Find("Hidden/Internal-Colored");
+			lineMaterial = new Material(shader);
+			lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+			// Turn on alpha blending
+			lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+			lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+			// Turn backface culling off
+			lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+			// Turn off depth writes
+			lineMaterial.SetInt("_ZWrite", 0);
+		}
+	}
+}
