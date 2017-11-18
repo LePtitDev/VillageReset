04851a7bdc2c8196b0f14134ef943bec27e550df
Lauriane <lauriane.loas@gmail.com>
2017-11-18 09:51:17 +0100

    pull

--- Assets/Scripts/WildAnimals/Animals.cs
+++ Assets/Scripts/WildAnimals/Animals.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Animals : MonoBehaviour {
 	private RaycastHit Hit;
-
-
+	private Moving Move;
+	private double life = 50 ;
 
 	/// Use this for initialization
 	private void Start () {
-		
+		Move = GetComponent<Moving>();
 	}
 	
 	// Update is called once per frame
 	public void Update () {
+		life = life - 0.05;
+		Debug.Log ("life" + life);
 		
 	}
 
@@ -21,13 +23,22 @@ public class Animals : MonoBehaviour {
 	// Avoid Objetcs (Tree,ore and don't go beyound limites of ground
 	public void AvoidObjects()
 	{
+		
+
 		transform.Translate (Vector3.forward * 2 * Time.deltaTime);
 
 		if (Physics.Raycast (transform.position, transform.TransformPoint(Vector3.forward), out Hit, 2) || !Manager.Instance.GetComponent<BoxCollider> ().bounds.Contains (transform.position)) {
-			//if true, random rotate bewteen 35 and 200 (avoid object)
+			//if true, random rotate bewteen 50 and 200 (avoid object)
 			transform.Rotate(Vector3.up * Random.Range(50,200));
 		}
 		
 	}
+
+	//avoid human
+	public void AvoidHuman()
+	{
+		
+	}
+
 
 }
--- Assets/Scripts/WildAnimals/Sheep.cs
+++ Assets/Scripts/WildAnimals/Sheep.cs
@@ -13,7 +13,6 @@ public class Sheep : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
 		animalS.AvoidObjects ();
 	}
 
