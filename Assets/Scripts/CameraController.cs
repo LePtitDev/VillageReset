738062f857ff54fdd9c0a50f0a73738874c9cad7
LePtitDev <perso@leptitdev.com>
2017-11-18 16:28:10 +0100

    Selection des entités + début de l'UI

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -26,49 +26,55 @@ public class CameraController : MonoBehaviour {
 	public float MinZoom;
 
 	// Target of the camera
-	GameObject target;
+	GameObject _target;
+	
+	// Target of the camera
+	public GameObject Target { get { return _target; } }
 
 	// The center of the camera lookAt
-	Vector3 center;
+	Vector3 _center;
 	// The camera rotation
-	float rotation;
+	float _rotation;
 	// The zoom level
-	float zoom;
+	float _zoom;
 
 	// Use this for initialization
-	void Start () {
-		target = null;
-		rotation = 0;
-		zoom = MinZoom;
-		center = MapCenter;
-		transform.position = MapCenter + new Vector3 (0f, 1f, -1f) * Distance * (zoom > 0 ? 1f / (1f + zoom) : 1f - zoom);
+	private void Start () {
+		_target = null;
+		_rotation = 0;
+		_zoom = MinZoom;
+		_center = MapCenter;
+		transform.position = MapCenter + new Vector3 (0f, 1f, -1f) * Distance * (_zoom > 0 ? 1f / (1f + _zoom) : 1f - _zoom);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	private void Update () {
 		if (Input.GetMouseButtonDown (0)) {
-			if (target != null) {
-				foreach (cakeslice.Outline o in target.GetComponentsInChildren<cakeslice.Outline>())
-					o.color = 0;
-				target = null;
-			}
+			_target = null;
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit, 100, LayerMask.GetMask("Agent"))) {
-				target = hit.collider.gameObject;
-				foreach (cakeslice.Outline o in target.GetComponentsInChildren<cakeslice.Outline>())
-					o.color = 1;
+			RaycastHit[] hits;
+			if ((hits = Physics.RaycastAll(ray)).Length != 0)
+			{
+				foreach (RaycastHit hit in hits)
+				{
+					if (hit.collider.gameObject.GetComponent<Entity>() != null)
+					{
+						_target = hit.collider.gameObject;
+						UIRefresh();
+						break;
+					}
+				}
 			}
 		}
 		if (Input.GetMouseButton (1))
-			rotation += Input.GetAxis ("Mouse X") * RotationSpeed;
-		if (Input.mouseScrollDelta.y != 0)
-			zoom = Mathf.Clamp (zoom + Input.mouseScrollDelta.y * ZoomSpeed, MinZoom > 1f ? MinZoom : -1f / MinZoom, MaxZoom < 1f ? -1f / MaxZoom : MaxZoom);
-		float rad = Mathf.Deg2Rad * rotation;
-		Vector3 newCenter = (target != null ? target.transform.position : center) - center;
+			_rotation += Input.GetAxis ("Mouse X") * RotationSpeed;
+		if (Input.mouseScrollDelta.y != 0.0f)
+			_zoom = Mathf.Clamp (_zoom + Input.mouseScrollDelta.y * ZoomSpeed, MinZoom > 1f ? MinZoom : -1f / MinZoom, MaxZoom < 1f ? -1f / MaxZoom : MaxZoom);
+		float rad = Mathf.Deg2Rad * _rotation;
+		Vector3 newCenter = (_target != null ? _target.transform.position : _center) - _center;
 		Vector3 forward = transform.rotation * Vector3.forward;
 		forward.y = 0;
-		if (target == null) {
+		if (_target == null) {
 			if (Input.GetKey (MoveForward))
 				newCenter += forward;
 			if (Input.GetKey (MoveBackward))
@@ -81,8 +87,31 @@ public class CameraController : MonoBehaviour {
 		newCenter.y = 0;
 		if (newCenter.magnitude > TranslationSpeed)
 			newCenter = newCenter.normalized * TranslationSpeed;
-		center += newCenter;
-		transform.position = center + new Vector3 (-Mathf.Sin(rad), (zoom >= 0 ? 1f / (1f + zoom) : 2f - 1f / (1f - zoom)), -Mathf.Cos(rad)) * Distance * (zoom > 0 ? 1f / (1f + zoom) : 1f - zoom);
-		transform.rotation = Quaternion.LookRotation (new Vector3 (Mathf.Sin(rad), -Mathf.Deg2Rad * 45f * (zoom >= 0 ? 1f / (1f + zoom) : 2f - 1f / (1f - zoom)), Mathf.Cos(rad)));
+		_center += newCenter;
+		transform.position = _center + new Vector3 (-Mathf.Sin(rad), (_zoom >= 0 ? 1f / (1f + _zoom) : 2f - 1f / (1f - _zoom)), -Mathf.Cos(rad)) * Distance * (_zoom > 0 ? 1f / (1f + _zoom) : 1f - _zoom);
+		transform.rotation = Quaternion.LookRotation (new Vector3 (Mathf.Sin(rad), -Mathf.Deg2Rad * 45f * (_zoom >= 0 ? 1f / (1f + _zoom) : 2f - 1f / (1f - _zoom)), Mathf.Cos(rad)));
 	}
+
+	// Refresh watchers
+	private void UIRefresh()
+	{
+		switch (_target.GetComponent<Entity>().Type)
+		{
+			case Entity.EntityType.RESSOURCE:
+				UIGetEntityInfo("Ressource").SetActive(true);
+				break;
+		}
+	}
+
+	// Get an entity info panel
+	private GameObject UIGetEntityInfo(string entityName)
+	{
+		foreach (Transform t in GameObject.Find("EntityInfo").GetComponentsInChildren<Transform>(true))
+		{
+			if (t.name == entityName)
+				return t.gameObject;
+		}
+		return null;
+	}
+	
 }
---
+++ Assets/Scripts/UI/RessourceWatcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RessourceWatcher : MonoBehaviour
+{
+
+	// Main camera
+	private CameraController _camera;
+
+	// Text for type
+	private Text _textType;
+
+	// Text for ressource
+	private Text _textQuantity;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_camera = Camera.main.GetComponent<CameraController>();
+		foreach (Text t in GetComponentsInChildren<Text>())
+		{
+			if (t.name == "Type")
+				_textType = t;
+			else if (t.name == "Quantity")
+				_textQuantity = t;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (_camera.Target == null)
+		{
+			gameObject.SetActive(false);
+			return;
+		}
+		Entity entity = _camera.Target.GetComponent<Entity>();
+		if (entity.Type != Entity.EntityType.RESSOURCE)
+		{
+			gameObject.SetActive(false);
+			return;
+		}
+		Ressource ressource = entity.GetComponent<Ressource>();
+		_textType.text = Enum.GetName(typeof(Ressource.RessourceType), ressource.Type);
+		_textQuantity.text = ressource.Count.ToString();
+	}
+}
